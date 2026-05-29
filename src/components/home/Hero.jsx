8d4eaee20cc182989f2e691c94e5b71e3ea580e1
unknown <imhyeongyun365@gmail.com>
2026-05-29 20:07:29 +0900

    아직 hero부분까지 못함

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,21 +3,45 @@ import "./styles/Hero.scss"
 const Hero = () => {
   return (
     <div className='inner hero-inner'>
+      <div className='intro-me'>
+        <img src="src\assets\hero-myphoto.jpg" alt="photo" />
+        <ul className='myspack'>
+          <li className='spack work'>
+            <span className='spack-num'>3+</span>
+            <span className='spack-name'>경력</span>
+          </li>
+          <li className='spack skills'>
+            <span className='spack-num'>3+</span>
+            <span className='spack-name'>기술</span>
+          </li>
+          <li className='spack projects'>
+            <span className='spack-num'>6+</span>
+            <span className='spack-name'>프로젝트</span>
+          </li>
+          <li className='spack Negotiation'>
+            <span className='spack-num'>1+</span>
+            <span className='spack-name'>협업 수</span>
+          </li>
+        </ul>
+      </div>
+      <div className='intro-title'>
+        <h1 className="tit">
+          <span className='main-title_1'>
+          Full-Stack Delveloper
+          </span>
+          <span className='main-title_2'>
+          풀스택 개발자 
+          </span>
+          <span className='main-title_3'>
+          임현균 입니다.
+          </span>
+        </h1>
 
-
-      <h1 className="tit">
-        <span className='main-title_1'>
-        Full-Stack Delveloper
-        </span>
-        <span className='main-title_2'>
-        풀스택 개발자 
-        </span>
-        <span className='main-title_3'>
-        임현균 입니다.
-        </span>
-      </h1>
-      
-      
+        <div className="intro-btn">
+          <button className='myworkBtn'>내 작업 보기</button>
+          <button className='mycallBtn'>연락하기</button>
+        </div>
+      </div>
     </div>
   )
 }
